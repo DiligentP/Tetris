36e0f48a2e15e5cc76ddc2c3f048ee35f7ef0300
DiligentP <31683152+DiligentP@users.noreply.github.com>
2017-09-13 14:36:29 +0900

    커밋

---
+++ Tetris/테트리스/Heder.h
@@ -0,0 +1,12 @@
+typedef struct tetris {
+
+	int level;       //레벨
+	int gmaeover;    //게임 오버
+	int score;       // 점수
+	int rotation;    // 도형의 위치
+	int absX, absY;  // 도형의 절대좌표
+	int curX, curY;  // 도형의 현재좌표
+	int curblock;    // 현재블럭
+	int nextblock;   // 다음블럭
+
+} Tetris;
---
+++ Tetris/테트리스/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void T_MAP();
+
+int MAP_X = 15;
+int MAP_Y = 20;
+
+
+int main() {
+
+	T_MAP();
+	
+	return 0;
+}
+void T_MAP() {
+	int i, j;
+	printf("\n\n    ");
+	///////////// Y //////////////////
+	for (i = 0; i < MAP_Y; i++) {
+
+		printf("\n    ¢Ì");
+		for (j = 0; j < (MAP_X - 2); j++) {
+			printf("  ");
+		}
+		printf("¢Ì");
+	}
+	///////////// X //////////////////
+	printf("\n    ");
+	for (i = 0; i < MAP_X; i++) {
+		printf("¢Ì");
+	}
+	//////////////////////////////////
+	printf("\n    ");
+}
