9ad764d3bc321ce0d8a37575d1a82cd3816d7233
DiligentP <31683152+DiligentP@users.noreply.github.com>
2017-09-19 00:39:58 +0900

    2017 09 19

--- Tetris/테트리스/main.c
+++ Tetris/테트리스/main.c
@@ -1,9 +1,4 @@
-/*#include <stdio.h>
-
-void T_MAP();
-
-int MAP_X = 15;
-int MAP_Y = 20;
+#include "Header.h"
 
 
 int main() {
@@ -12,23 +7,3 @@ int main() {
 	
 	return 0;
 }
-void T_MAP() {
-	int i, j;
-	printf("\n\n    ");
-	///////////// Y //////////////////
-	for (i = 0; i < MAP_Y; i++) {
-
-		printf("\n    ¢Ì");
-		for (j = 0; j < (MAP_X - 2); j++) {
-			printf("  ");
-		}
-		printf("¢Ì");
-	}
-	///////////// X //////////////////
-	printf("\n    ");
-	for (i = 0; i < MAP_X; i++) {
-		printf("¢Ì");
-	}
-	//////////////////////////////////
-	printf("\n    ");
-}*/
---
+++ Tetris/테트리스/system.c
@@ -0,0 +1,25 @@
+#include "Header.h"
+
+int MAP_X = 15;
+int MAP_Y = 20;
+
+void T_MAP() {
+	int i, j;
+	printf("\n\n    ");
+	///////////// Y //////////////////
+	for (i = 0; i < MAP_Y; i++) {
+
+		printf("\n    ¢Ì");
+		for (j = 0; j < (MAP_X - 2); j++) {
+			printf("  ");
+		}
+		printf("¢Ì");
+	}
+	///////////// X //////////////////
+	printf("\n    ");
+	for (i = 0; i < MAP_X; i++) {
+		printf("¢Ì");
+	}
+	//////////////////////////////////
+	printf("\n    ");
+}
